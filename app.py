acdff0825542f4b68c1641f5e5a1ff2f3783aac4
James <jdowns06@gmail.com>
2024-03-21 11:57:30 -0500

    Init commit

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect
+from azure.communication.email import EmailClient
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/send-feedback', methods=['POST'])
+def send_feedback():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    
+    send_email(name, email, message)
+    return redirect('/')
+
+def send_email(name, email, message):
+    try:
+        # Azure Communication Services setup
+        # connection_string = os.getenv('AZURE_EMAIL_CONNECTION_STRING')
+        connection_string = "endpoint=https://comm-flaskappci-prod-01.unitedstates.communication.azure.com/;accesskey=Wx4IqSaGbKE0/DLqLmstXjrF5ixYHVAKQyhhteh9yf6vAwIuTPcf9Ldu/3WQfpPnSmWxaujMSew6cdSgQXdVSw=="
+        client = EmailClient.from_connection_string(connection_string)
+        
+        message = {
+            "senderAddress": "DoNotReply@fd42e661-b94d-4d0d-9d9c-692c9273a2be.azurecomm.net",
+            "recipients":  {
+                "to": [{"address": "jdowns06@gmail.com" }],
+            },
+            "content": {
+                "subject": "Flask App Feedback",
+                "plainText": f"Name: {name} \nEmail: {email} \nMessage: {message}",
+            }
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+
+    except Exception as ex:
+        print(ex)    
+
+if __name__ == '__main__':
+    app.run(debug=True)
